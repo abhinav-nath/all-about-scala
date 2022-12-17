0653512149d8e925d46a56b1e75f1b726edf006b
Abhinav Nath <abhinavnath@ymail.com>
2022-12-17 15:23:15 +0530

    Update ValuesVariablesTypes

--- code/src/main/scala/classes/ValuesVariablesTypes.scala
+++ code/src/main/scala/classes/ValuesVariablesTypes.scala
@@ -3,6 +3,9 @@ package classes
 
 object ValuesVariablesTypes extends App {
 
+  // --- Values (constants, immutable) ---
+  // Prefer vals over vars
+
   val x: Int = 42
   println(x)
 
@@ -11,6 +14,19 @@ object ValuesVariablesTypes extends App {
   val y = 21 // compiler can infer types
   println(y.getClass)
 
-  val str: String = "hello"; // semi-colons are allowed but optional
+  val aString: String = "hello"; // semi-colons are allowed but optional
+
+  val aBoolean: Boolean = false
+  val aChar: Char = 'x'
+  val anInt: Int = x
+  val aShort: Short = 1234
+  val aLong: Long = 3423421312312312L
+  val aFloat: Float = 3.14f
+  val aDouble: Double = 22.76
+
+  // --- Variables (mutable) ---
+  var aVariable: Int = 4
+
+  aVariable = 5 // variables can be re-assigned unlike vals
 
 }
